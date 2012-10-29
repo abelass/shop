07fcdb4078dc569a718e629e1dd4fdf1224391ed
abelass <websolutions@mychacra.net>
2012-10-29 09:40:01 +0100

    import

---
+++ base/shop_tables_principales.php
@@ -0,0 +1,84 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+//
+// Formulaires : Structure
+//
+
+function shop_declarer_tables_principales($tables_principales){
+	$spip_shop_commande = array(
+		"id_commande" 			=> "int(21) NOT NULL",
+		"id_panier" 			=> "int(21) NOT NULL",		
+		"nom_societe" 			=> "varchar(255) NOT NULL",		
+		"prenom" 				=> "varchar(255) NOT NULL",
+		"nom_famille" 			=> "varchar(255) NOT NULL",		
+		"email" 				=> "varchar(255) NOT NULL",
+		"adresse" 				=> "text NOT NULL",		
+		"code_postal" 			=> "varchar(255) NOT NULL",
+		"telephone" 			=> "varchar(255) NOT NULL",		
+		"ville" 				=> "text NOT NULL",
+		"pays" 					=> "int(3) NOT NULL",		
+		"id_produit" 			=> "text NOT NULL",
+		"conteneur" 			=> "varchar(50) NOT NULL",
+		"numero_tva" 			=> "varchar(255) NOT NULL",
+		"commentaire" 			=> "text NOT NULL",
+		"montant" 				=> "float NOT NULL",
+		"frais_livraison" 		=> "float NOT NULL",
+		"date_livraison" 		=> "date DEFAULT '0000-00-00' NOT NULL",
+		"adresse_livraison" 	=> "varchar(255) NOT NULL",
+		"code_postal_livraison" => "varchar(255) NOT NULL",
+		"ville_livraison" 		=> "text NOT NULL",
+		"pays_livraison" 		=> "int(3) NOT NULL",	
+		"date_livraison" 		=> "date DEFAULT '0000-00-00' NOT NULL",						
+		"code_devise" 		=> "varchar(3) NOT NULL",
+		"statut" 				=> "varchar(255) NOT NULL",	
+		"type_paiement" 		=> "varchar(55) NOT NULL",
+		"statut_paiement" 		=> "varchar(255) NOT NULL",
+		"traitement" 			=> "varchar(255) NOT NULL",		
+		"date_enregistrement" 	=> "date DEFAULT '0000-00-00' NOT NULL",		
+		"date_creation" 		=> "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",		
+		"maj" 					=> "TIMESTAMP");
+	
+	$spip_shop_commande_key = array(
+		"PRIMARY KEY" 	=> "id_commande",
+		"KEY id_panier" 	=> "id_panier",		
+		);
+		
+	$spip_shop_commande_join = array(
+		"id_commande"	=> "id_commande",
+		"id_panier"	=> "id_panier",		
+		);
+
+	$tables_principales['spip_shop_commande'] = array(
+		'field' => &$spip_shop_commande,
+		'key' => &$spip_shop_commande_key,
+		'join' => &$spip_shop_commande_join
+	);
+
+	$spip_shop_prix = array(
+		"id_prix" 	=> "int(21) NOT NULL",
+		"id_objet" 	=> "int(21) NOT NULL",
+		'objet' => 'varchar(25) not null default ""',		
+		"code_devise" 	=> "varchar(3) NOT NULL",
+		"prix" 		=> "float (38,2) NOT NULL",
+		);
+	
+	$spip_shop_prix_key = array(
+		"PRIMARY KEY" 	=> "id_prix",
+		"KEY id_objet"	=> "id_objet",
+		);
+		
+	$spip_shop_prix_join = array(
+		"id_prix"	=> "id_prix",
+		"id_objet"	=> "id_objet",
+		);
+
+	$tables_principales['spip_shop_prix'] = array(
+		'field' => &$spip_shop_prix,
+		'key' => &$spip_shop_prix_key,
+		'join' => &$spip_shop_prix_join
+	);
+	
+	return $tables_principales;
+	
+	}
+?>
---
+++ formulaires/boutique_fr.php
@@ -0,0 +1,59 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé  genere le NOW()
+// langue / language = fr
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// A
+'adresse' => "Adresse",
+
+// b
+'shop' => "Boutique",
+
+// c
+'choisir' => "Sélectionner",
+'choix_devises' => "Choix des devises",
+'choix_devise' => "Devise",
+'choix_rubrique' => "Choix la rubrique",
+'commande' => "Commande",
+'comment' => "Commentaires",
+'constellation' => "Constellation",
+
+
+// d
+'date_enregistrement' => "Date enregistrement",
+'date_vente' => "Date vente",
+'detail_commande' => "Détail commande",
+'devises_choisis' => "Devises choisis",
+'devise_default' => "Devise par defaut",
+
+// e
+'email' => "Email",
+
+// f
+'frais_livraison' => "Frais livraison",
+
+// i
+'info_generales' => "Informations générales",
+'info_prix' => "Prix",
+'info_produit' => "Informations produits",
+
+// p
+'payer_paypal' => "Payer via Paypal",
+'nom_etoile' => "Nom étoile",
+
+
+// p
+'paiement' => "Paiement",
+'paiement_valide' => "Votre paiement a &eacute;t&eacute; valid&eacute;, le coffret vous sera envoy&eacute; dans les plus brefs d&eacute;lais",
+'paiement_probleme' => "Votre paiement n'a pas &eacute;t&eacute; effectu&eacute;, veuillez v&eacute;rifier dans votre compte paypal ou contactez nous.",
+'produit' => "Produit",
+
+// r
+'rubrique_produits' => "Rubrique des produits",
+
+// v
+'ville' => "Ville",
+
+);
+?>
