60d7283596f61a85d48f9d0b084bd73c5fcad6ae
abelass <websolutions@mychacra.net>
2012-11-07 16:57:40 +0100

    toutes les déclarations ensemble

--- base/shop.php
+++ base/shop.php
@@ -1,8 +1,13 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
-//
-// Formulaires : Structure
-//
+
+function shop_tables_interfaces($tables_interfaces){
+
+    $tables_interfaces['table_des_tables']['shop_prix'] = 'shop_prix';
+    $tables_interfaces['table_des_tables']['shop_commande'] = 'shop_commande';
+    $tables_interfaces['table_des_tables']['geo_pays'] = 'geo_pays';    
+    return $tables_interfaces;
+}
 
 function shop_declarer_tables_principales($tables_principales){
 	$spip_shop_commande = array(
@@ -81,4 +86,53 @@ function shop_declarer_tables_principales($tables_principales){
 	return $tables_principales;
 	
 	}
+
+function shop_declarer_tables_auxiliaires($tables_auxiliaires){
+
+    $spip_tokens_retour = array(
+        "id_token_retour"           => "bigint(21) NOT NULL",
+        "id_commande"           => "bigint(21) NOT NULL",
+        "id_paiement"           => "bigint(21) NOT NULL",
+        "token_retour"      => "VARCHAR(255) NOT NULL",
+        "token_retour"      => "VARCHAR(255) NOT NULL",
+        "token_panier"  =>  "VARCHAR(255) NOT NULL",
+        "prestataire_paiement" => "VARCHAR(255) NOT NULL"
+        );
+
+    $spip_tokens_retour_key = array(
+        "PRIMARY KEY"       => "id_token_retour",
+        "KEY token_retour"  => "token_retour",
+        "KEY token_panier"  => "token_panier"
+        );  
+        
+
+    $spip_tokens_retour_join = array(
+        "token_panier"      => "token_panier"
+        );  
+    
+    $tables_auxiliaires['spip_shop_tokens_retour'] = array(
+        'field' => &$spip_tokens_retour,
+        'key' => &$spip_tokens_retour_key,
+        'join' => &$spip_tokens_retour_join
+    );
+            
+    $spip_geo_pays = array(
+        "id_pays"   => "bigint(21) NOT NULL",
+        "nom"       => "VARCHAR(255) NOT NULL",
+        );
+
+    $spip_geo_pays_key = array(
+        "PRIMARY KEY"       => "id_pays",
+        );  
+        
+    
+    $tables_auxiliaires['spip_geo_pays'] = array(
+        'field' => &$spip_geo_pays,
+        'key' => &$spip_geo_pays_key,
+    );          
+    
+
+return $tables_auxiliaires;
+}
+
 ?>
