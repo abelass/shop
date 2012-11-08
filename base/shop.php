8304c81e49412d65abcfaa1e220f2865722da811
abelass <websolutions@mychacra.net>
2012-11-08 09:59:05 +0100

    un rien

--- base/shop.php
+++ base/shop.php
@@ -6,6 +6,7 @@ function shop_tables_interfaces($tables_interfaces){
     $tables_interfaces['table_des_tables']['shop_prix'] = 'shop_prix';
     $tables_interfaces['table_des_tables']['shop_commande'] = 'shop_commande';
     $tables_interfaces['table_des_tables']['geo_pays'] = 'geo_pays';    
+    
     return $tables_interfaces;
 }
 
