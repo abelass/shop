089f8fed3475541d53c115f1af3124642a5f14f8
abelass <websolutions@mychacra.net>
2012-12-21 18:00:46 +0100

    ajouter type_paiement dans commandes paiements

---
+++ base/shop.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Déclinaisons Produit
+ * (c) 2012 Rainer Müller
+ * Licence GNU/GPL
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function shop_declarer_tables_principales($tables_principales){
+
+        $tables_principales['spip_commandes']['field']['type_paiement']= "varchar(50) NOT NULL";
+
+        return $tables_principales;
+
+}
+
+?>
---
+++ shop_administrations.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Signaler des abus
+ * (c) 2012 My Chacra
+ * Licence GNU/GPL
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+include_spip('inc/cextras');
+include_spip('base/wiki_cso');
+
+/**
+ * Fonction d'installation du plugin et de mise à jour.
+ * Vous pouvez :
+ * - créer la structure SQL,
+ * - insérer du pre-contenu,
+ * - installer des valeurs de configuration,
+ * - mettre à jour la structure SQL 
+**/
+function shop_upgrade($nom_meta_base_version, $version_cible) {
+	$maj = array();
+	$maj['create'] = array(array('maj_tables', array('spip_commandes')));
+	$maj['1.0.4'] = array(array('maj_tables', array('spip_commandes')));    
+                           
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+?>
