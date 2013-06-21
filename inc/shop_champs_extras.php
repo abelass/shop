769cc30ad188fbac3f1bb92eee5bce04b7fc467e
Rainer Müller <rainer@websimple.be>
2013-06-21 20:12:43 +0200

    enlever les champs teste

--- inc/shop_champs_extras.php
+++ inc/shop_champs_extras.php
@@ -53,36 +53,6 @@ function inc_shop_champs_extras_dist($defaut=array()){
                         'afficher_si' => '@commande_commentaire@ == "on"',
                     )
                 ), 
-                array(
-                    'saisie' => 'oui_non',// Pour la configuration
-                    'saisie_2' => 'textarea', // Pour le formulaire public                   
-                    'formulaires' => array('configuer_shop','editer_client'),
-                    'tables'=>array(
-                        'spip_commandes'=>array(
-                            'field'=>array(
-                                'teste'=>"text NOT NULL"
-                                )
-                             )
-                        ),
-                    'options' => array(
-                        'nom' => 'commande_teste',// Pour la configuration
-                        'nom_2' => 'teste',// Pour le formulaire public                          
-                        'label' => _T('shop:label_teste'),
-                        'defaut' => $defaut['commande_teste']
-                    )
-                ),
-                array(
-                    'saisie' => 'checkbox',
-                    'formulaires' => array('configuer_shop'),
-                    'options' => array(
-                        'class'=>'float_right',
-                        'datas'=>array('on'=>_T('item_oui')),
-                        'nom' => 'commande_teste_obligatoire',
-                        'label' => _T('saisies:option_obligatoire_label'),
-                        'defaut' => $defaut['commande_teste_obligatoire'],
-                        'afficher_si' => '@commande_teste@ == "on"',
-                    )
-                ), 
               
               )
                                
