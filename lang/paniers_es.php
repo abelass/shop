ebb59a6a39981d9be7e655c0a1a988d199eeddfc
Rainer Müller <rainer@websimple.be>
2013-07-15 16:58:15 +0200

    traductions du panier

---
+++ lang/paniers_es.php
@@ -0,0 +1,67 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de 
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_ajouter' => 'Añadir a la cesta',
+
+	// C
+	'configurer_titre' => 'Configurar el plugin cesta',
+
+	// D
+	'description_noisette' => 'Mostrar la minicesta(Número de artículos et precio total)',
+	'dont_total_taxe' => 'Total impuestos incluidos',
+
+	// L
+	'limite_enregistres_label' => 'Visitantes registrados',
+	'limite_ephemere_label' => 'Visitantes no-registrados',
+	'limite_explication' => 'Introduzca la duración de las cestas (en horas) por tipo de visitantes',
+	'limite_titre' => 'Duración de la cesta',
+
+	// M
+	'mon_panier' => 'Mi cesta',
+	'mon_panier_modifier' => 'Modificar mi cesta',
+	'mon_panier_voir' => 'Ver mi cesta',
+
+	// N
+	'noisette_label_afficher_lien_page_panier' => 'Afficher un lien vers la page spip.php?page=panier ?', # NEW
+	'noisette_label_afficher_titre_noisette' => 'Afficher un titre ?', # NEW
+	'noisette_label_niveau_titre' => 'Choisir le niveau du titre', # NEW
+	'noisette_label_titre_noisette' => 'Titre :', # NEW
+	'noisette_label_titre_noisette_perso' => 'Si titre personnalisé :', # NEW
+	'noisette_titre_perso' => 'Titre personnalisé', # NEW
+	'nom_bouton_plugin' => 'Cestas',
+	'nom_noisette' => 'Minicesta',
+
+	// P
+	'panier_description' => 'Descripción',
+	'panier_erreur_quantites' => 'Las cantidades deben ser números integrales',
+	'panier_montant' => 'Precio',
+	'panier_prix_unitaire' => 'Precio por unidad',
+	'panier_quantite' => 'Cantidad',
+	'panier_quantite_ok' => 'La cantidades han sido cambiados',
+	'panier_recalculer' => 'Re-calcular',
+	'panier_taxes' => 'Impuestos',
+	'panier_total_ht' => 'Total sin IVA',
+	'panier_total_ttc' => 'Total',
+	'panier_valider' => 'Validar mi cesta',
+	'panier_vide' => 'La cesta está vacía',
+	'paniers' => 'Cestas',
+	'produit_un' => '1 producto',
+	'produits_plusieurs' => '@nb@ productos',
+
+	// T
+	'titre_mon_panier' => 'Mi cesta',
+	'titre_panier' => 'Cesta',
+
+	// V
+	'vider_le_panier' => 'Vaciar la cesta',
+	'voir_mon_panier' => 'Mostrar mi cesta'
+);
+
+?>
