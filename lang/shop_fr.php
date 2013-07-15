c9234cd81cb877b4df43740cfd6213b4d91a953d
Rainer Müller <rainer@websimple.be>
2013-07-15 17:30:56 +0200

    nettoyage

--- lang/shop_fr.php
+++ lang/shop_fr.php
@@ -4,110 +4,6 @@
 // langue / language = fr
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// A
-'adresse' => "Adresse",
-'annule' => "Annulé",
-'alerte_action' => "Cette action est irréversible, voulez-vous vraiment continuer ?",
-'attente' => "En attente",
-'aucune_commande_traitement' => 'Il n\'y a actuellement aucune commande en traitement',
-'ajouter_prix' => 'Ajouter un prix',
-
-// b
-'bonjour' => "Bonjour",
-'byby' => "Nous vous remercions pour votre commande.<br />Bien à vous,",
-
-// c
-
-'cfg_titre_authentification'=>'Authentifiaction',
-'cfg_titre_authentification_automatique'=>'Authentifiaction automatique après l\'inscription',
-'cfg_titre_champs_commandes'=>'Champs formulaire commandes',
-'cfg_titre_shop' => "Configuration Shop",
-'choisir' => "Sélectionner",
-'choix_prix' => "Le prix du produit",
-'choix_produit' => "Choissisez un produit",
-'choix_rubrique' => "Choix la rubrique",
-'code_postal' => "Code postal",
-'commande' => "Commande",
-'commander_produit_rubrique' => "Commander un produit",
-'commandes' => "Commandes",
-'commande_enregistre' => "La commande suivante a été enregistrée",
-'commandes_traite' => "Commandes traitées",
-'commande_traite' => "Archiver la commande",
-'commandes_traitement' => "Commandes à traiter",
-'comment' => "Message",
-
-
-
-// d
-'date_enregistrement' => "Date d'enregistrement",
-'date_vente' => "Date de vente",
-'de_la_part_de' => "De la part de",
-'detail_commande' => "Détails de la commande",
-
-
-// e
-'email' => "Email",
-'echoue' => "Echoué",
-'expire' => "Expiré",
-'explication_authentification_automatique' => "Pouvoir accéder directement à la confirmation des données enregistrés lors d'une nouvelle inscription sans devoir se connecter préablement",
-'explication_champs_commandes' => "Choissisez les champs suivant si vous voulez les inclure dans le formulaire de commande",
-'explication_confirmation' => "voici les informations que vous avez encodées",
-
-// f
-'frais_livraison' => "Frais de livraison",
-
-// i
-'info_generales' => "1. Informations générales",
-'infos_livraison'=> "2. Informations de livraison",
-'infos_personnelles' => "Informations personnelles",
-'info_prix' => "Prix",
-'info_pays' => "Pays",
-'info_produit' => "Informations produits",
-
-//l
-'label_commentaire'=>'Commentaire :',
-'liste_de_commandes'=>'Liste des commandes',
-
-// m
-'message_paiement_client' => "Votre paiement n'a pas encore été validé; la raison :",
-'message_paiement_webmaster' => "Le paiement n'a pas encore été validé; la raison :",
-'message_validation_accepte_client' => "votre paiement a été accepté",
-'message_validation_accepte_webmaster' => "Le paiement a été accepté",
-'message_validation_inconnue' => "raison inconnu",
-
-
-// p
-'paye'=> "payé",
-'payer_paypal' => "Payer via Paypal",
-'paiement' => "Paiement de la commande",
-'pendant' => "En cours",
-'produit' => "Produit",
-'produits' => "Produits",
-'paiement_description' => "Veuillez sélectionner le mode de paiement",
-'paiement_probleme' => "Votre paiement n'a pas encore été validé, veuillez vérifier dans votre compte Paypal ou contacter le webmaster du site",
-'paiement_valide' => "Votre paiement a été validé",
-
-
-
-// r
-'rembourse_partiellement' => "Remboursé partiellement",
-'rejete' => "Rejeté",
-'rembourse' => "Remboursé",
-'retour_panel' => "Retour au panel",
-
-// s
-'shop' => "Shop",
-'statut_paiement' => "Statut",
-'supprimer' => "Supprimer",
-
-// t
-'titre_confirmation' => "Confirmation de votre commande",
-'titre_produit' => "Nom du produit",
-'traitement' => "Traitement",
-
-// v
-'ville' => "Ville",
-
 
 //A
 'acces_facture'=>'Facture',
@@ -118,23 +14,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'annuler'=>'<< Annuler',
 
 
+//B
+'bonjour' => "Bonjour",
+
+
 //C
-'commande'=>'Commande',
-'commande_invalide'=>'Référence de commande invalide',
-'commande_ref'=>'Commande @ref@',
-'commande_ref_statut'=>'Votre commande @ref@ est @statut@',
-'confirmation_commande'=>'Confirmation de votre commande',
-'confirmation_la_commande'=>'Confirmation de la commande',
+'cfg_titre_authentification'=>'Authentifiaction',
+'cfg_titre_authentification_automatique'=>'Authentifiaction automatique après l\'inscription',
+'cfg_titre_champs_commandes'=>'Champs formulaire commandes',
 'continuer'=>'Continuer >>',
 'coordonnees'=>'Coordonnées',
 
-
 //D
-'deja_inscrit'=>'Déja inscrit',
-'detail_commande'=>'Détails',
+'deja_client'=>'Si vous avez déjà un compte client, identifiez-vous',
+'detail_commande' => "Détails de la commande",
 
+
+//E
+'explication_authentification_automatique' => "Pouvoir accéder directement à la confirmation des données enregistrés lors d'une nouvelle inscription sans devoir se connecter préablement",
+'explication_champs_commandes' => "Choissisez les champs suivant si vous voulez les inclure dans le formulaire de commande",
+
+//I
+'info_prix' => "Prix",
+'info_pays' => "Pays",
 
 //L
+'label_commentaire'=>'Commentaire :',
+'liste_de_commandes'=>'Liste des commandes',
 'les_produits'=>'Les produits',
 'les_articles_precieux'=>'Articles en vente',
 'les_documents_precieux' => 'Documents',
@@ -155,26 +61,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'paiement_fictif'=>'Paiement fictif',
 'passer_la_commande'=>'Passer la commande',
 
-// Q
-'quantite'=>'Quantité',
-//S
-'total'=>'Total',
-'deja_client'=>'Si vous avez déjà un compte client, identifiez-vous',
+//R
+'retour_panel' => "Retour au panel",
 
-//U
-'une_commande_sur'=>'Une commande sur @nom@',
+//S
+'shop' => "Shop",
 
 //V
 'verifiez_vos_coordonnees'=>'Vérifiez vos coordonnées',
 'veuillez_vous_connecter'=>'Veuillez d\'abord vous connecter',
 'vider_le_panier'=>'Vider le panier',
-'vos_commandes_payees'=>'Vos commandes payées',
 'vos_coordonnees'=>'Si vous n\'avez pas de compte client, saisissez vos coordonnées',
-'votre_commande'=>'Votre commande',
 'votre_commande_du'=>'Votre commande du ',
-'votre_commande_sur'=>'Votre commande sur @nom@',
-'vous_inscrire'=>'Vous inscrire',
-
-
 );
 ?>
