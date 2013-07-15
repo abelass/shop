28f52f1b6a4b9a6248cac50087d0fe0fd42075e4
Rainer Müller <rainer@websimple.be>
2013-07-15 17:46:40 +0200

    traductions español

---
+++ lang/shop_es.php
@@ -0,0 +1,113 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé  genere le NOW()
+// langue / language = fr
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// A
+
+'aucune_commande_traitement' => 'Il n\'y a actuellement aucune commande en traitement',
+
+
+// b
+'bonjour' => "Hola",
+
+
+// c
+
+'cfg_titre_authentification'=>'Autentificación',
+'cfg_titre_authentification_automatique'=>'Authentifiaction automatique après l\'inscription',
+'cfg_titre_champs_commandes'=>'Champs formulaire commandes',
+
+// d
+
+'detail_commande' => "Detalle del pedido",
+
+
+// e
+'explication_authentification_automatique' => "Pouvoir accéder directement à la confirmation des données enregistrés lors d'une nouvelle inscription sans devoir se connecter préablement",
+'explication_champs_commandes' => "Choissisez les champs suivant si vous voulez les inclure dans le formulaire de commande",
+
+
+
+// i
+'info_prix' => "Precio",
+
+//l
+'label_commentaire'=>'Comentario :',
+'liste_de_commandes'=>'Lista de los pedidos',
+
+
+// p
+'paiement' => "Paiement de la commande",
+
+// r
+'retour_panel' => "Retour au panel",
+
+// s
+'shop' => "Shop",
+
+
+//A
+'acces_facture'=>'Facture',
+'adresse_de'=>'Adresse de',
+'aucun_produit'=>'Aucun produit',
+'aucune_commande_valide'=>'Il n\'y a (actuellement) aucune commande.',
+'ajouter_au_panier'=>'Ajouter au panier',
+'annuler'=>'<< Annuler',
+
+
+//C
+'commande'=>'Commande',
+'commande_invalide'=>'Référence de commande invalide',
+'commande_ref'=>'Commande @ref@',
+'commande_ref_statut'=>'Votre commande @ref@ est @statut@',
+'confirmation_commande'=>'Confirmation de votre commande',
+'confirmation_la_commande'=>'Confirmation de la commande',
+'continuer'=>'Continuer >>',
+'coordonnees'=>'Coordonnées',
+
+
+//D
+'deja_inscrit'=>'Déja inscrit',
+'detail_commande'=>'Détails',
+
+
+//L
+'les_produits'=>'Les produits',
+'les_articles_precieux'=>'Articles en vente',
+'les_documents_precieux' => 'Documents',
+
+//M
+'merci_de_votre_commande'=>'Nous vous remercions de votre commande @ref@',
+'mes_commandes'=>'Mes commandes',
+'modifier_adresse'=>'Modifier',
+'modifiez_vos_coordonnees'=>'Modifiez vos coordonnées',
+'mon_compte' => 'Mon compte',
+
+//N
+'nouvelle_adresse_de' => 'Nouvelle adresse de',
+
+//P
+'panier'=>'Panier',
+'paiement'=>'Paiement',
+'passer_la_commande'=>'Formalizar pedido',
+
+
+//S
+'total'=>'Total',
+'deja_client'=>'Si vous avez déjà un compte client, identifiez-vous',
+
+//U
+'une_commande_sur'=>'Une commande sur @nom@',
+
+//V
+'verifiez_vos_coordonnees'=>'Vérifiez vos coordonnées',
+'veuillez_vous_connecter'=>'Veuillez d\'abord vous connecter',
+'vos_commandes_payees'=>'Vos commandes payées',
+'vos_coordonnees'=>'Si vous n\'avez pas de compte client, saisissez vos coordonnées',
+'votre_commande_du'=>'Votre commande du ',
+
+
+);
+?>
