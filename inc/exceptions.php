577d7238ea98e9af1170643d90cb20bdeffaf80c
Rainer <rainer@websimple.be>
2015-09-23 18:15:29 +0200

    format

--- inc/exceptions.php
+++ inc/exceptions.php
@@ -3,16 +3,16 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /*Les exceptions*/
 function inc_exceptions_dist($filtrer=''){
-    $exceptions=array(
-        'objet'=>array('site'=>'syndic','syndic'=>'syndic'),
-        'titre'=>array(
-            'auteur'=>'nom',
-            'site'=>'nom_site',
-            'syndic'=>'nom_site',
-            ),
-        );
-    $retour=$exceptions;   
-    if($filtrer)$retour=$exceptions[$filtrer];
-    return $retour;
+  $exceptions=array(
+  'objet'=>array('site'=>'syndic','syndic'=>'syndic'),
+    'titre'=>array(
+      'auteur'=>'nom',
+      'site'=>'nom_site',
+      'syndic'=>'nom_site',
+    ),
+  );
+  $retour=$exceptions;   
+  if($filtrer)$retour=$exceptions[$filtrer];
+  return $retour;
 }
 ?>
